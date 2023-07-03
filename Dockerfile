d1a5902bc14b680aa3e1e812a0f4472aa017d5ec
SalimAliev <calim250899@mail.ru>
2023-07-03 21:04:57 +0300

    Secon commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.10
+
+RUN mkdir -p /usr/src/app/
+WORKDIR /usr/src/app/
+
+COPY . /usr/src/app/
+
+CMD ["python", "script.py"]
--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 from random import choice
 
-# Список слов для игры
-words = ['машина', 'самолет', 'велосипед']
+# Получение списка слов
+with open('words.txt', 'r', encoding='UTF-8') as file:
+    words = file.read().split()
 
 HANGMAN = (
     """
@@ -77,9 +78,9 @@ HANGMAN = (
 )
 
 
-
+print('V1')
 print('Приветствую тебя игрок')
-user_name = input('Введи свое имя:')
+user_name = input('Введи свое имя:').strip()
 
 while len(user_name) > 10:
     user_name = input('Введено слишком длинное имя игрока, введите пожалуйста имя длиной не более 10 символов:')
@@ -89,24 +90,24 @@ if len(user_name) == 0:
 
 print('Правила игры:\n'
       f'В игре загадано слово, игроку необходимо его угадать. Игрок должен укадывать буквы из загаданного слова. '
-      f'При каждой невенерной букве на виселице рисуется деталь приближающая к проигрышу. У игрока есть {max_wrong} '
+      f'При каждой невенерной букве на виселице рисуется деталь приближающая к проигрышу. У игрока есть {len(HANGMAN)} '
       f'неверных попыток.')
 
 
 while True:
-    game_status = input('Готовы начать игру (да/нет):')
+    game_status = input('Готовы начать игру (да/нет):').strip()
 
-    if game_status == 'да':
+    if game_status.lower() == 'да':
         random_word = list(choice(words))
         wrong_count = 0
         max_wrong = len(HANGMAN) - 1
         hidden_word = ['-']*len(random_word)
         entered_letter = []
 
-        while wrong_count < max_wrong:
+        while True:
 
             if wrong_count == max_wrong:
-                print(f'К созажалению {user_name} вы проиграли')
+                print(f'К созажалению {user_name} вы проиграли, угадываемое словов {"".join(random_word)}')
                 break
 
             elif random_word == hidden_word:
@@ -116,11 +117,15 @@ while True:
             print(f'{HANGMAN[wrong_count]}\n'
                   f'Слово: {"".join(hidden_word)}\n'
                   f'Ошибок: {wrong_count}\n'
-                  f'Названные буквы: {", ".join(entered_letter)}')
-            letter = input('Введи букву: ')
+                  f'Названные буквы: {", ".join(entered_letter)}\n')
+            letter = input('Введи букву: ').strip()
 
-            if len(letter) > 1 and letter.isalnum():
+            if len(letter) > 1 or not letter.isalpha():
                 print('Необоходимо ввести один символ авлофита')
+                continue
+
+            if letter.lower() in entered_letter:
+                print('Вы уже вводили эту букву')
                 continue
 
             entered_letter.append(letter)
@@ -132,7 +137,7 @@ while True:
                     hidden_word[index] = letter
                     wrong_flag = True
 
-            if wrong_flag == False:
+            if not wrong_flag:
                 wrong_count += 1
 
     elif game_status == 'нет':
